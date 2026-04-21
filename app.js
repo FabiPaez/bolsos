ad680363d7d2597ddb2e7d153a5493465248f961
FabiPaez <89799622+FabiPaez@users.noreply.github.com>
2026-04-21 20:28:13 -0300

    prueba

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 const { createApp } = Vue;
 
-const vuetify = Vuetify.createVuetify();
-
 createApp({
-  data() {
-    return {
-      productos: [
-        {
-          nombre: "Bolso 1",
-          imagenes: ["bolso1-1.jpg"]
-        }
-      ]
-    }
-  },
-  template: `
-    <v-app>
-      <v-container>
-        <h1>Catálogo OK</h1>
-      </v-container>
-    </v-app>
-  `
-}).use(vuetify).mount('#app');
+  template: `<h1>Funciona</h1>`
+}).mount('#app');
