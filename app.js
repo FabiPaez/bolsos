8e24d4611425cb5ec32505ac2c3c75390fe39fab
FabiPaez <89799622+FabiPaez@users.noreply.github.com>
2026-04-29 18:05:42 -0300

    Agregando titulos

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const { createApp } = Vue;
+const { createVuetify } = Vuetify;
 
-const vuetify = Vuetify.createVuetify();
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#e8aeb7',
+          secondary: '#f7d1cd',
+          accent: '#d6a4a4'
+        }
+      }
+    }
+  }
+});
 
 const BASE_URL = "https://raw.githubusercontent.com/FabiPaez/bolsos/main/bolsos/";
 
@@ -15,9 +29,30 @@ createApp({
         {
           nombre: "Bolso Casual",
           imagenes: ["bolso2-1.jpg", "bolso2-2.jpg"]
+        },{
+          nombre: "Bolso Fashion",
+          imagenes: ["bolso1-1.jpg", "bolso1-2.jpg"]
+        },{
+          nombre: "Anteojos",
+          imagenes: ["bolso1-1.jpg", "bolso1-2.jpg"]
+        },{
+          nombre: "Mochila",
+          imagenes: ["bolso1-1.jpg", "bolso1-2.jpg"]
+        },{
+          nombre: "Bolso Elegante",
+          imagenes: ["bolso1-1.jpg", "bolso1-2.jpg"]
         }
       ]
     };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }, 100);
   },
 
   methods: {
@@ -31,15 +66,27 @@ createApp({
 
   template: `
     <v-app>
-      <v-container>
+    <v-app-bar elevation="2" color="white">
+      <v-toolbar-title class="title-bar" style="color:#6b4c57">
+         Catálogo de Bolsos
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container "mt-6">
+        <div class="hero-title">
+           Bolsos que enamoran  
+        </div>
+        <div class="hero-subtitle">
+          Elegancia, estilo y diseño para cada ocasión
+        </div>
         <v-row>
           <v-col
             v-for="(prod, index) in productos"
             :key="index"
             cols="12" sm="6" md="4"
           >
-            <v-card>
-              <v-carousel height="300" cycle>
+            <v-card class="card-hover">
+              <v-carousel height="280" cycle>
                 <v-carousel-item
                   v-for="(img, i) in getImages(prod.imagenes)"
                   :key="i"
@@ -47,22 +94,25 @@ createApp({
                 />
               </v-carousel>
 
-              <v-card-title>{{ prod.nombre }}</v-card-title>
-
-              <v-card-actions>
-                <v-btn
-                  color="pink"
-                  :href="whatsappLink(prod.nombre)"
-                  target="_blank"
-                  block
-                >
-                  Consultar por WhatsApp
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
+              <v-card-title class="text-center title-style">
+                {{ prod.nombre }}
+                  </v-card-title>
+                    <v-card-actions class="d-flex justify-center pb-4">
+                      <v-btn
+                        class="whatsapp-btn"                        
+                        :href="whatsappLink(prod.nombre)"
+                        target="_blank"
+                        width="75%"
+                        prepend-icon="mdi-whatsapp"
+                      >
+                        Consultar por WhatsApp
+                      </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
       </v-container>
+    </v-main>
     </v-app>
   `
 }).use(vuetify).mount('#app');
